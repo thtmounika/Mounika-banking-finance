89c23fa5bf40e5c43fb4adf32da8544bb816ce72
thtmounika <71330114+thtmounika@users.noreply.github.com>
2024-06-10 19:15:47 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,48 +1,133 @@
----
-- hosts: all
-  become: true
-  tasks:
-  - name: install promethus server
-    shell: | 
-      sudo apt update 
-      sudo apt install docker.io -y 
-      wget https://github.com/prometheus/prometheus/releases/download/v2.44.0-rc.2/prometheus-2.44.0-rc.2.linux-amd64.tar.gz 
-      tar -xvzf prometheus-2.44.0-rc.2.linux-amd64.tar.gz 
-      cd prometheus-2.44.0-rc.2.linux-amd64
-      sudo docker run -d --name node-exporter -p 9100:9100 prom/node-exporter 
-      echo '
-      # my global config
-      global:
-        scrape_interval: 15s
-        evaluation_interval: 15s
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
 
-      alerting:
-        alertmanagers:
-          - static_configs:
-              - targets:
-                # - alertmanager:9093
-
-      rule_files:
-
-      scrape_configs:
-        - job_name: "prometheus"
-          static_configs:
-            - targets: ["localhost:9090"]
-        - job_name: "node-exporter"
-          static_configs:
-            - targets: ["localhost:9100"]
-      ' > prometheus.yml
-      nohup ./prometheus > prometheus.log 2>&1 &
-  
-
-  - name: install grafana server
-    shell: |
-      sudo apt-get install -y apt-transport-https
-      sudo apt-get install -y software-properties-common wget
-      sudo wget -q -O /usr/share/keyrings/grafana.key https://apt.grafana.com/gpg.key
-      echo "deb [signed-by=/usr/share/keyrings/grafana.key] https://apt.grafana.com stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
-      sudo apt-get update
-      sudo apt-get install grafana -y 
-      sudo systemctl daemon-reload
-      sudo systemctl start grafana-server
-      sudo systemctl enable grafana-server
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-south-1"
+}
+# create vpc 
+resource "aws_vpc" "mounikavpc" {
+  cidr_block = "10.0.0.0/16"
+}
+# Create an internet gateway
+resource "aws_internet_gateway" "mounikagw" {
+  vpc_id = aws_vpc.mounikavpc.id
+  tags = {
+    Name = "gateway1"
+  }
+}
+# Create a custom Route Table
+resource "aws_route_table" "mounika-rt" {
+  vpc_id = aws_vpc.mounikavpc.id
+route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.mounikagw.id
+}
+    tags = {
+     Name = "mounika-rt1"
+    }
+}
+# Create a Subnet
+resource "aws_subnet" "mounika-subnet" {
+  vpc_id     = aws_vpc.mounikavpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "ap-south-1b"
+  tags = {
+    Name = "subnet1"
+  }
+}
+# Associate the Subnet with the Route Table
+resource "aws_route_table_association" "mounika-rt-sub-assoc" {
+  subnet_id      = aws_subnet.mounika-subnet.id
+  route_table_id = aws_route_table.mounika-rt.id
+}
+# Create a security group 
+resource "aws_security_group" "mounika-sg" {
+  name        = "mounika-sg"
+  description = "Enable web traffic for the project"
+  vpc_id      = aws_vpc.mounikavpc.id
+  ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+}
+ egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+}
+  ingress {
+    description = "HTTPS traffic"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+ }
+  ingress {
+    description = "HTTPS traffic"
+    from_port        = 0
+    to_port          = 6500
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+ }
+  ingress {
+    description = "HTTPS traffic"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+ }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+ tags = {
+    Name = "mounika-sg1"
+  }
+}
+ # Network Interface Setup
+resource "aws_network_interface" "mounika-ni" {
+  subnet_id       = aws_subnet.mounika-subnet.id
+  private_ips     = ["10.0.1.10"]
+  security_groups = [aws_security_group.mounika-sg.id]
+ }
+ # Attach an elastic Ip to network interface 
+resource "aws_eip" "mounika-eip" {
+ vpc = true
+network_interface = aws_network_interface.mounika-ni.id
+associate_with_private_ip ="10.0.1.10"
+ }
+ # Creating an ubuntu EC2 instance
+resource "aws_instance" "prod-server" {
+  ami = "ami-0f58b397bc5c1f2e8"
+  instance_type = "t2.micro"
+  availability_zone = "ap-south-1b"
+  key_name = "terraform"
+  network_interface {
+    device_index = 0
+    network_interface_id = aws_network_interface.mounika-ni.id
+  }
+  user_data = <<-EOF
+  #!/bin/bash
+     sudo apt-get update -y
+     sudo apt install docker.io -y
+   https://docs.google.com/document/d/1nn6X6jSoZvDc_WXRtuqeiS1kJSOj1SJH/edit?usp=drive_link&ouid=103157204066713600014&rtpof=true&sd=true
+  sudo systemctl enable docker
+     sudo docker run -itd -p 8085:8081 thtmounikadocker/mentorbank:1
+     sudo docker start $(docker ps -aq)
+  EOF
+  tags = {
+    Name = "prod-server"
+  }
+  }
